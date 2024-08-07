ced062e4fcfdefb563100bc77fbecffdff749e5e
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2024-08-07 00:02:33 -0500

    connected to mongo db public routes needed to stated in middleware

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -19,7 +19,6 @@
 //   return cached.conn;
 // }
 
-
 // import mongoose from 'mongoose';
 
 // const MONGODB_URI: string | undefined = process.env.MONGODB_URI;
@@ -28,7 +27,7 @@
 //   throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
 // }
 
-// /** 
+// /**
 //  * Global is used here to maintain a cached connection across hot reloads in development.
 //  * This prevents connections growing exponentially during API Route usage.
 //  */
@@ -65,43 +64,48 @@
 //   return cached.conn;
 // }
 
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const MONGODB_URI: string | undefined = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local"
+  );
 }
 
-/** 
+/**
  * Global is used here to maintain a cached connection across hot reloads in development.
  * This prevents connections growing exponentially during API Route usage.
  */
-let cached = (global as any).mongoose;
+let cached: any = (global as any).mongoose;
 
 if (!cached) {
   cached = (global as any).mongoose = { conn: null, promise: null };
 }
 
-export async function connectToDatabase() {
+export const connectToDatabase = async () => {
   if (cached.conn) {
-    console.log('Using cached MongoDB connection');
+    console.log("Using cached MongoDB connection");
     return cached.conn;
   }
 
   if (!cached.promise) {
     const opts = {
       bufferCommands: false,
-      dbName: 'geaux337app',
+      dbName: "geaux337app",
     };
 
-    cached.promise = mongoose.connect(MONGODB_URI as string, opts).then((mongoose) => {
-      console.log('MongoDB connected');
-      return mongoose;
-    }).catch((error) => {
-      console.error('MongoDB connection error:', error);
-      throw error;
-    });
+    cached.promise = cached.promise || mongoose
+      .connect(MONGODB_URI as string, opts)
+      .then((mongoose) => {
+        console.log("MongoDB connected");
+        return mongoose;
+      })
+      .catch((error) => {
+        console.error("MongoDB connection error:", error);
+        throw error;
+      });
   }
 
   try {
@@ -112,10 +116,7 @@ export async function connectToDatabase() {
   }
 
   return cached.conn;
-}
-
-
-
+};
 
 //****
 //**IF DATABASE ISSUES OCCUR PUT THIS SCRIPT IN API/TEST-CONNECTION/ROUTE.TS
